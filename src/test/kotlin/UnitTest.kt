c8117bf98f18e42aaed3335d0c9aa390dcd01c6d
Matias Holte <matias.holte@cognite.com>
2024-02-02 11:27:36 +0100

    add method for listing unit systems

--- src/test/kotlin/UnitTest.kt
+++ src/test/kotlin/UnitTest.kt
@@ -139,4 +139,12 @@ class UnitTest {
             unitService.getUnitsByQuantityAndAlias("Temperature", "unknown")
         }
     }
+
+    @Test
+    fun lookupUnitSystem() {
+        val unitService = UnitService.service
+        assertEquals(
+            setOf("Default", "Imperial", "SI"), unitService.getUnitSystems(),
+        )
+    }
 }

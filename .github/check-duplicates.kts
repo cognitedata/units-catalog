0eab3e1ff946ffbaf9357ea1e90b33a0461d86d9
Jacob Eliat-Eliat <jacob.eliat-eliat@cognite.com>
2024-05-13 12:24:35 +0200

    trying to add a way to check the duplicates

---
+++ .github/check-duplicates.kts
@@ -0,0 +1,10 @@
+@file:DependsOn("file://${System.getenv("JAR_PATH")}")
+import com.cognite.units.UnitService
+
+fun main() {
+    val service = UnitService.service
+    val result = service.checkForDuplicateConversions()  // Assume someMethod is a method you want to invoke
+    println("duplicate conversion: $result")
+}
+
+main()

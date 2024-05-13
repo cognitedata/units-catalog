409968994d8caffddfb58d0c27eb5c4ee761cf21
Jacob Eliat-Eliat <jacob.eliat-eliat@cognite.com>
2024-05-13 15:32:46 +0200

    this does not work, apparently kotlin scripts don't recognize transitive depenencies

--- .github/check-duplicates.main.kts
+++ .github/check-duplicates.main.kts
@@ -1,9 +1,15 @@
 import com.cognite.units.UnitService
+import com.cognite.units.Conversion
+import com.cognite.units.TypedUnit
 
 fun main() {
     val service = UnitService.service
-    val result = service.checkForDuplicateConversions()  // Assume someMethod is a method you want to invoke
-    println("duplicate conversion: $result")
+    val result: Map<String, Map<Conversion, List<TypedUnit>>> = service.getDuplicateConversions(service.getUnits())  // Assume someMethod is a method you want to invoke
+    result.keys.forEach { outerKey ->
+        println("Quantity with duplicates: $outerKey")
+        result[outerKey]?.keys?.forEach { innerKey: Conversion ->
+        }
+    }
 }
 
 main()
